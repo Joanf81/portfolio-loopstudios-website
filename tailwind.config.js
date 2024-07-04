8d896af068f99bc3bbbe5f28f3d634beeadc751a
Joan Fernández Bornay <joanf81@gmail.com>
2024-07-04 17:44:07 +0200

    Added hero & description sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '576px',
+      'md': '769px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      backgroundImage: {
+        'hero-image-desktop': "url('/images/desktop/image-hero.jpg')",
+        'hero-image-mobile': "url('/images/mobile/image-hero.jpg')"
+      }
+    },
   },
   plugins: [],
 }
